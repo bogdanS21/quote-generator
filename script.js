ce552ba0aabde51a51dc41610e6637c636fcd578
Bogdan Seliman <selimanbogdan@gmail.com>
2022-07-12 22:32:50 +0300

    Styling and some JS

---
+++ script.js
@@ -0,0 +1,16 @@
+let apiQuotes = [];
+
+
+// Get quotes from api
+
+async function getQuotes() {
+    const apiUrl = "https://type.fit/api/quotes";
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        console.log(apiQuotes)
+    } catch (error) {
+        alert(error)
+    }
+}
+getQuotes();
