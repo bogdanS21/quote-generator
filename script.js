3891854c0161feda1be6ee9551c5a1e24ce87ae6
Bogdan Seliman <selimanbogdan@gmail.com>
2022-07-13 12:08:54 +0300

    Finished.

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote")
+const authorText = document.getElementById("author")
+const twitterBtn = document.getElementById("twitter")
+const newQuoteBtn = document.getElementById("new-quote")
+
+//Get all quotes 
 let apiQuotes = [];
+
+//Random quotes and set value..
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    //Check if Author is blank and replace it with 'Unknown'
+
+    if (!quote.author) {
+        authorText.textContent = "Unknown"
+    } else {
+        authorText.textContent = quote.author;
+    }
+    quoteText.textContent = quote.text;
+}
+
 
 
 // Get quotes from api
@@ -8,9 +30,16 @@ async function getQuotes() {
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes)
+        newQuote();
     } catch (error) {
         alert(error)
     }
 }
+
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank')
+}
+
+
 getQuotes();
