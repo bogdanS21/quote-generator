73d9daec1b5a544f0fb000e96f012205194aaa39
Bogdan Seliman <selimanbogdan@gmail.com>
2022-07-13 16:36:44 +0300

    Finish.

--- script.js
+++ script.js
@@ -4,13 +4,27 @@ const authorText = document.getElementById("author")
 const twitterBtn = document.getElementById("twitter")
 const newQuoteBtn = document.getElementById("new-quote")
 
+const loader = document.getElementById('loader')
+
+
+
 //Get all quotes 
 let apiQuotes = [];
+
+
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+function completeLoading() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
 
 //Random quotes and set value..
 function newQuote() {
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
-
     //Check if Author is blank and replace it with 'Unknown'
 
     if (!quote.author) {
@@ -19,6 +33,7 @@ function newQuote() {
         authorText.textContent = quote.author;
     }
     quoteText.textContent = quote.text;
+
 }
 
 
@@ -26,10 +41,12 @@ function newQuote() {
 // Get quotes from api
 
 async function getQuotes() {
+    loading();
     const apiUrl = "https://type.fit/api/quotes";
     try {
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
+        completeLoading();
         newQuote();
     } catch (error) {
         alert(error)
